f3fe8935a6bacd7ab939b69db847441f8c9aaf8b
Shane West <shane.west@regalcinemas.com>
2021-02-17 15:40:29 -0500

    Add google ads.

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ const Home = () => {
     <>
       <Head>
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+        <script data-ad-client="ca-pub-8578215930386365" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
       </Head>
       <article>
         <h1>{title}</h1>
